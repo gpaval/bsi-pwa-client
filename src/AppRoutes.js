2f988789fd2b6bdd078a69c2a55ad6ae484b1bbc
Andrei <andrei.vasilache@assist.ro>
2020-10-25 11:20:34 +0200

    Initial react commit

---
+++ src/AppRoutes.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Switch></Switch>
+    </Router>
+  );
+}
+
+export default AppRoutes;
