39f3ec9ed4b89e7ad48527d2ca99cc05776e4850
Andrei <andrei.vasilache@assist.ro>
2020-10-26 20:19:07 +0200

    Added Homepage footer

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -5,6 +5,9 @@ import StyledHomepage from "./StyledHomepage";
 import identifyIcon from "../../assets/images/identifyIcon.svg";
 import yourDataIcon from "../../assets/images/yourDataIcon.svg";
 
+import logoutIcon from "../../assets/images/logoutIcon.svg";
+import notificationsIcon from "../../assets/images/notificationsIcon.svg";
+
 const Homepage = () => {
   // todo: update when the API will be implemented.
 
@@ -32,6 +35,25 @@ const Homepage = () => {
             text={"Your data"}
             onCardClick={() => console.log("done")}
           ></CardComponent>
+        </div>
+
+        <div className="homepage__footer">
+          <div className="homepage--footer">
+            <img
+              src={logoutIcon}
+              className="homepage--footer__logo"
+              alt="Log out"
+            />
+            <div className="homepage--footer__title">Log-out</div>
+          </div>
+          <div className="homepage--footer">
+            <img
+              src={notificationsIcon}
+              className="homepage--footer__logo"
+              alt="Log out"
+            />
+            <div className="homepage--footer__title">Notifications</div>
+          </div>
         </div>
       </div>
     </StyledHomepage>
--- src/pages/Homepage/StyledHomepage.js
+++ src/pages/Homepage/StyledHomepage.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import colors from "../../constants/themingConstants";
 
 const StyledHomepage = styled.div`
+  display: flex;
+  justify-content: center;
   .homepage {
     .homepage-title {
       font-weight: 700;
@@ -21,6 +23,28 @@ const StyledHomepage = styled.div`
     }
     &__card {
       margin-bottom: 58px;
+    }
+
+    &__footer {
+      display: flex;
+      justify-content: space-between;
+      margin-top: 100px;
+
+      .homepage--footer {
+        display: flex;
+        align-items: center;
+
+        &__logo {
+          display: block;
+          margin-left: auto;
+          margin-right: auto;
+        }
+
+        &__title {
+          font-size: 14px;
+          font-weight: 400;
+        }
+      }
     }
   }
 `;
